52c11dd066495e0defdad9d87dbbc6a107aa338a
Grzegorz Kunc <grzeg.kunc@gmail.com>
2021-10-22 18:33:29 +0200

    Not working XSS attack Level 1

--- XSS-Platform/src/app/app.module.ts
+++ XSS-Platform/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { NavItemComponent } from './nav-item/nav-item.component';
 import { Level1Component } from './levels/level1/level1.component';
 import { Level2Component } from './levels/level2/level2.component';
 import { Level3Component } from './levels/level3/level3.component';
+import { FormsModule } from '@angular/forms';
+import { SafePipe } from './safe-pipe.pipe';
 
 @NgModule({
   declarations: [
@@ -17,10 +19,12 @@ import { Level3Component } from './levels/level3/level3.component';
     Level1Component,
     Level2Component,
     Level3Component,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- XSS-Platform/src/app/levels/level1/level1.component.ts
+++ XSS-Platform/src/app/levels/level1/level1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-level1',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./level1.component.scss']
 })
 export class Level1Component implements OnInit {
-
+  @Input() providedString: string = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  verify(): void {
+    document.getElementsByClassName('testDiv')[0].innerHTML = this.providedString;
+  }
 }

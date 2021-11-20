d5e2e7630034d78ad77f550b4e27aba67908947d
Grzegorz Kunc <grzeg.kunc@gmail.com>
2021-11-20 14:16:10 +0100

    Remove node_modules

--- XSS-Platform/src/app/levels.ts
+++
@@ -1,7 +0,0 @@
-export class Level {
-  levelRoute: string = '';
-  levelNumber: number = 0;
-  isCompleted: boolean = false;
-  isSelected: boolean = false;
-  numberOfHints: number = 0;
-}
--- XSS-Platform/src/app/score.actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-export const increment = createAction('[Score Component] Increment', props<{ byScore: number }>());
-export const reset = createAction('[Score Component] Reset');

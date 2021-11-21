77053e227c2eb8f851acecf9855c43ba7c2f1e53
Benedykt Roszko <JXWG36@motorolasolutions.com>
2021-11-21 15:11:29 +0100

    endpoint implemented

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+import io
+
+from flask import Flask, send_file
 
 from db.DbConnector import DbConnector
-from utils.utils import DB_NAME
+from utils.utils import DB_NAME, create_zip_filename_from
 
 app = Flask(__name__)
 
@@ -15,7 +17,16 @@ def hello(name):
 def test_db():
     db_connector = DbConnector(DB_NAME)
     test_id, name = db_connector.read_test_data()
+    db_connector.close_connection()
     return f"id: {test_id}, name: {name}"
+
+
+@app.route('/files/<int:zip_id>')
+def get_zip_file(zip_id):
+    db_connector = DbConnector(DB_NAME)
+    binary_zip_file = db_connector.get_zip_file(create_zip_filename_from(zip_id))
+    db_connector.close_connection()
+    return send_file(io.BytesIO(binary_zip_file[0]), mimetype="application/zip")
 
 
 if __name__ == '__main__':
--- backend/db/DbConnector.py
+++ backend/db/DbConnector.py
@@ -9,3 +9,10 @@ class DbConnector:
     def read_test_data(self):
         self.cur.execute("select * from tests")
         return self.cur.fetchone()
+
+    def get_zip_file(self, file_name):
+        self.cur.execute("select binary from zip_tests where filename = ?", (file_name,))
+        return self.cur.fetchone()
+
+    def close_connection(self):
+        self.con.close()
